c681c70bac0eb7af24e170774004b64ec52f2525
Kishanth S <154578200+KishanthSelvaraj@users.noreply.github.com>
2024-12-28 10:53:44 +0530

    Update Popular.jsx

--- ecommerce/src/components/Popular.jsx
+++ ecommerce/src/components/Popular.jsx
@@ -1,31 +1,47 @@
 import React, { useEffect, useState } from "react";
-// import POPULAR from "../assets/popular";
 import Item from "./Item";
 
 const Popular = () => {
   const [popular_products, setPopular_products] = useState([]);
+  const [loading, setLoading] = useState(true); // Loader state
 
-  useEffect(()=>{
-    fetch("https://ecommerce-mern-w5oy.onrender.com/popularproducts").then((res)=>res.json()).then((data)=>setPopular_products(data))
-  },[])
+  useEffect(() => {
+    fetch("https://ecommerce-mern-w5oy.onrender.com/popularproducts")
+      .then((res) => res.json())
+      .then((data) => {
+        setPopular_products(data);
+        setLoading(false); // Hide loader once data is fetched
+      });
+  }, []);
+
   return (
     <section className="bg-primary">
       <div className="max_padd_container py-12 xl:py-28 xl:w-[88%]">
         <h3 className="h3 text-center">Popular Products</h3>
         <hr className="h-[3px] md:w-1/2 mx-auto bg-gradient-to-1 from-transparent via-black to-transparent mb-16" />
-        {/* container */}
-        <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-6">
-          {popular_products.map((item) => (
-            <Item
-              key={item.id}
-              id={item.id}
-              image={item.image}
-              name={item.name}
-              new_price={item.new_price}
-              old_price={item.old_price}
-            />
-          ))}
-        </div>
+        {loading ? (
+          // Loader
+          <div
+            className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-75"
+            style={{ zIndex: 1000 }}
+          >
+            <div className="w-8 h-8 border-4 border-red-500 border-t-transparent rounded-full animate-spin"></div>
+          </div>
+        ) : (
+          // Container
+          <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-6">
+            {popular_products.map((item) => (
+              <Item
+                key={item.id}
+                id={item.id}
+                image={item.image}
+                name={item.name}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
