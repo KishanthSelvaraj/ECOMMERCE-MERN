d5fff589f93e51b381bf544955788b6b4161b672
Kishanth S <154578200+KishanthSelvaraj@users.noreply.github.com>
2024-12-28 10:40:57 +0530

    Update Hero.jsx

--- ecommerce/src/components/Hero.jsx
+++ ecommerce/src/components/Hero.jsx
@@ -6,17 +6,15 @@ const Hero = () => {
   return (
     <section className="relative bg-hero bg-cover  bg-center bg-no-repeat h-screen w-full ">
       <div className="max_padd_container relative top-32 xs:top-52 ">
-        <h1 className="h1 capitalize max-w-[37rem] text-white">
-          <span className="text-[#6af771]">Sustainable</span> Spring 2024
-        </h1>
-        <p className="text-white regular-20 mt-3 lg:mt-6 max-w-[36rem] ">
-          Spring is in the air, and it's the perfect time to refresh your home
-          and lifestyle with eco-friendly solutions. At <span className="text-gray-500">Sustainable.co.za</span> we
-          are celebrating the season of renewal with incredible promotions that
-          will help you save energy, reduce your carbon footprint, and embrace a
-          more sustainable way of living.
-        </p>
-        {/* <div className="flexStart !items-center gap-x-4 my-10">
+      <h1 className="h1 capitalize max-w-[37rem] text-white">
+  <span className="text-[#f26161]">Eco-Friendly</span> Solutions for You
+</h1>
+<p className="text-white regular-20 mt-3 lg:mt-6 max-w-[36rem]">
+  Discover the perfect way to refresh your home and lifestyle with innovative, eco-friendly products. Enjoy incredible deals that help you save energy while making a positive impact on the planet.
+</p>
+
+
+        <div className="flexStart !items-center gap-x-4 my-10">
           <div className="!regular-24 flexCenter gap-x-3 text-white">
             <FaStar />
             <FaStar />
@@ -27,7 +25,7 @@ const Hero = () => {
             176k
             <span className="regular-16 sm:regular-20"> Excellent Reviews</span>
           </div>
-        </div> */}
+        </div>
         <div className="max-xs:flex-col lg:flex gap-2 hidden mt-4 lg:mt-10">
           {/* <NavLink className={"btn_white_rounded flexCenter "}>Shop now</NavLink> */}
           <NavLink
